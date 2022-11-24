0b92f4e066fffa66624d24dfdd3ecb33d46f7d8c
GabrielDBT <gabrielbtegani@gmail.com>
2022-11-24 12:49:17 -0300

    Collections and Functions (Dart)

---
+++ lib/1_funcoes/1_funcoes.dart
@@ -0,0 +1,9 @@
+int somaInt(int num1, int num2) {
+  print('Executando a soma de inteiros($num1, $num2)');
+  return num1 + num2;
+}
+
+void main() {
+  int valorCalculado = somaInt(25, 32);
+  print('A soma dos dois é $valorCalculado');
+}
